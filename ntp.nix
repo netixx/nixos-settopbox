a352a2a3bf725d2897b22e5faa622f34d0f97de3
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2018-07-21 20:43:20 +0200

    Correction configuration NTP

--- ntp.nix
+++ ntp.nix
@@ -10,8 +10,9 @@
     config = {config, pkgs, lib, ...}: {
       services.chrony = {
         enable = true;
-        servers = [ "127.127.0.1" ];
+        servers = [ "127.127.1.0" ];
         extraConfig = ''
+          allow 127.0.0.0/8
           allow 10.0.0.0/8
           local stratum 12
         '';
